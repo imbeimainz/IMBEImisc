8d980eecc7fb675e1bc62eeee2e6646bc1dd9d1a
Federico Marini <marinif@uni-mainz.de>
2019-08-27 11:20:17 +0200

    imports fixed

--- R/IMBEImisc-pkg.R
+++ R/IMBEImisc-pkg.R
@@ -1,6 +1,9 @@
 #' IMBEImisc
 #'
 #' `IMBEImisc` is an R package that provides
+#' 
+#' @importFrom graphics abline par plot points text
+#' @importFrom ROCR performance prediction
 #'
 #' @author Federico Marini \email{marinif@@uni-mainz.de}
 #' @name IMBEImisc-pkg
